05182c22c5762d97e97f58dbe33dccdade6d1266
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-07-15 18:30:59 +0200

    feat: add pages, home, create, detail page

--- notes-app/frontend/src/App.tsx
+++ notes-app/frontend/src/App.tsx
@@ -1,5 +1,21 @@
+// 1. pages
+// 2.
+
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import CreatePage from "./pages/CreatePage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
