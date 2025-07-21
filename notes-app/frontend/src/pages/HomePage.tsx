985f6eac85dcc40e3f926fe22be131e48ad4d4e1
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-07-21 13:13:11 +0200

    feat: rate limit ui

--- notes-app/frontend/src/pages/HomePage.tsx
+++ notes-app/frontend/src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 import Navbar from "../components/Navbar";
+import RateLimitedUi from "../components/RateLimitedUi";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+
   return (
     <div className="min-h-screen">
       <Navbar />
+      {isRateLimited && <RateLimitedUi />}
     </div>
   );
 };
