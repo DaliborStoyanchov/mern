be8e105fe290c3047ea48c3969b7349aaf46c088
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-07-22 10:59:03 +0200

    feat: fetch using axiox, jsonplaceholder

--- notes-app/frontend/src/pages/HomePage.tsx
+++ notes-app/frontend/src/pages/HomePage.tsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Navbar from "../components/Navbar";
 import RateLimitedUi from "../components/RateLimitedUi";
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error fetching notes");
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
     <div className="min-h-screen">
