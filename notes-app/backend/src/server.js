e9d92aa6c6515aa41664154e0a61b23198d57d90
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-08-06 19:33:32 +0200

    feat: nodemon, test crud endpoits

--- notes-app/backend/src/server.js
+++ notes-app/backend/src/server.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.get("/notes", (req, res) => {
+  res.status(200).send("You got 5 notes");
+});
+
+app.post("/notes", (req, res) => {
+  res.status(201).json({ message: "Post created successfully!" });
+});
+
+app.put("/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully!" });
+});
+
+app.delete("/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post deleted successfully!" });
+});
+
 app.listen(5001, () => {
   console.log("Server is up and running at PORT: 5001");
 });
