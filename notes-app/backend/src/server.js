6a81a951bc5b4cd2d53e60229d0b0c008bbec4c0
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-08-11 22:23:14 +0200

    feat: optimizing folder structure - notesRoutes

--- notes-app/backend/src/server.js
+++ notes-app/backend/src/server.js
@@ -1,22 +1,9 @@
 import express from "express";
+import notesRoutes from "../routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/notes", (req, res) => {
-  res.status(200).send("You got 5 notes");
-});
-
-app.post("/notes", (req, res) => {
-  res.status(201).json({ message: "Post created successfully!" });
-});
-
-app.put("/notes/:id", (req, res) => {
-  res.status(200).json({ message: "Post updated successfully!" });
-});
-
-app.delete("/notes/:id", (req, res) => {
-  res.status(200).json({ message: "Post deleted successfully!" });
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
   console.log("Server is up and running at PORT: 5001");
