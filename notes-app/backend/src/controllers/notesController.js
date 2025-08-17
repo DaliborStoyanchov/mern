e645ccc2a086452174cfef9afc54939f83e4b31f
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-08-17 17:45:00 +0200

    feat:add  controller logic - get notes,  create note, update note

--- notes-app/backend/src/controllers/notesController.js
+++ notes-app/backend/src/controllers/notesController.js
@@ -1,15 +1,60 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You just fetched the notes");
+import Note from "../models/Note.js";
+
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-export const createNote = (req, res) => {
-  res.status(201).json({ message: "Note created successfully!" });
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    const note = new Note({
+      title,
+      content,
+    });
+
+    const savedNote = await note.save();
+
+    res.status(201).json({ message: "Note created successfully!", savedNote });
+  } catch (error) {
+    console.error("Error in createNote controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-export const updateNote = (req, res) => {
-  res.status(200).send({ message: "Note updated successfully!" });
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        content,
+      },
+      { new: true }
+    );
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+
+    res
+      .status(200)
+      .json({ message: "Note updated successfully!", updatedNote });
+  } catch (error) {
+    console.error("Error in updateNote controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-export const deleteNote = (req, res) => {
+export const deleteNote = async (req, res) => {
   res.status(200).send({ message: "Note updated successfully!" });
 };
