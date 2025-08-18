a3331f23c0a4cb4945dc7c10cd79767fdda24dae
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-08-18 21:51:15 +0200

    feat: add get note by id controller, update logic for controllers

--- notes-app/backend/src/controllers/notesController.js
+++ notes-app/backend/src/controllers/notesController.js
@@ -1,12 +1,26 @@
 import Note from "../models/Note.js";
 
-export const getAllNotes = async (req, res) => {
+export const getAllNotes = async (_, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ createdAt: -1 });
 
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json({ message: "Note found successfully!", note });
+  } catch (error) {
+    console.error("Error in getNote controller", error);
 
     res.status(500).json({ message: "Internal server error" });
   }
@@ -56,5 +70,18 @@ export const updateNote = async (req, res) => {
 };
 
 export const deleteNote = async (req, res) => {
-  res.status(200).send({ message: "Note updated successfully!" });
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+
+    res
+      .status(200)
+      .json({ message: "Note deleted successfully!", deletedNote });
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
