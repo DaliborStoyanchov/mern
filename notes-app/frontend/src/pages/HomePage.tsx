1303132a0847c04181cf2273deb2cd9e43efa632
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-09-06 17:56:51 +0200

    feat: add notes not found component

--- notes-app/frontend/src/pages/HomePage.tsx
+++ notes-app/frontend/src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import RateLimitedUi from "../components/RateLimitedUi";
 import NoteCard from "../components/NoteCard";
 import toast from "react-hot-toast";
 import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 type Notes = { title: string; _id: number; createdAt: string };
 
@@ -16,8 +17,6 @@ const HomePage = () => {
     const fetchNotes = async () => {
       try {
         const res = await api.get("/notes");
-
-        console.log(res.data);
 
         setNotes(res.data);
       } catch (error: any) {
@@ -41,7 +40,9 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
+
       {isRateLimited && <RateLimitedUi />}
+
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {isLoading && (
           <div className="text-center text-primary py-10">LOADING NOTES...</div>
