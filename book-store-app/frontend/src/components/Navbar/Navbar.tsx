bc548a36843df6165fe3b406c7c23a294a2dfe30
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-09-26 20:18:03 +0200

    feat: add react-icon, add icon to navbar

--- book-store-app/frontend/src/components/Navbar/Navbar.tsx
+++ book-store-app/frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+import { HiOutlineBars3CenterLeft } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+
 const Navbar = () => {
   return (
-    <div>
-      <div className="!font-primary">This is Montserrat</div>
-
-      <div className="!font-secondary">This is Roboto</div>
-    </div>
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        <div>
+          <Link to="/">
+            <HiOutlineBars3CenterLeft className="size-6" />
+          </Link>
+          <div>
+            <IoSearchOutline />
+            <input
+              type="text"
+              placeholder="Search here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+        <div>Nav Items</div>
+      </nav>
+    </header>
   );
 };
 
