ede76a2d1b6c46cc4d1bbbb30c156efd810f3867
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-09-28 03:18:51 +0200

    feat: add navbar user conditional

--- book-store-app/frontend/src/components/Navbar/Navbar.tsx
+++ book-store-app/frontend/src/components/Navbar/Navbar.tsx
@@ -1,25 +1,55 @@
 import { Link } from "react-router-dom";
-import { HiOutlineBars3CenterLeft } from "react-icons/hi2";
+import {
+  HiOutlineBars3CenterLeft,
+  HiOutlineHeart,
+  HiOutlineShoppingCart,
+  HiOutlineUser,
+} from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
 
 const Navbar = () => {
+  const currentUser = true;
+
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
-        <div>
+        <div className="flex items-center md:gap-16 gap-4">
           <Link to="/">
             <HiOutlineBars3CenterLeft className="size-6" />
           </Link>
-          <div>
-            <IoSearchOutline />
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoSearchOutline className="absolute inline-block left-2 inset-y-2" />
             <input
               type="text"
-              placeholder="Search here"
+              placeholder="What are you looking for?"
               className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
             />
           </div>
         </div>
-        <div>Nav Items</div>
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <Link to="/">
+                <p>user</p>
+              </Link>
+            ) : (
+              <Link to="/">
+                <HiOutlineUser className="size-6" />
+              </Link>
+            )}
+          </div>
+
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+          <Link
+            to="/"
+            className="bg-[#FFCE1A] p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <HiOutlineShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
--- book-store-app/frontend/tailwind.config.js
+++ book-store-app/frontend/tailwind.config.js
@@ -7,6 +7,12 @@ export default {
         sans: ["Montserrat", "ui-sans-serif", "system-ui"],
         secondary: ["Roboto", "ui-sans-serif", "system-ui"],
       },
+      colors: {
+        primary: "#FFCE1A",
+        secondary: "#0d0842",
+        bg: "#F3F3F3",
+        favorite: "#FF5841",
+      },
     },
   },
   plugins: [],
