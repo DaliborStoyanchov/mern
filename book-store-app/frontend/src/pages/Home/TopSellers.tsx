a53520fd2e781eeb40bc1f1bc3fb1ae1cb9a71ec
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-10-04 23:23:39 +0200

    feat: add top sellers category

---
+++ book-store-app/frontend/src/pages/Home/TopSellers.tsx
@@ -0,0 +1,31 @@
+const categories = [
+  "Choose a genre",
+  "Business",
+  "Fiction",
+  "Horror",
+  "Adventure",
+];
+
+const TopSellers = () => {
+  return (
+    <div className="py-10">
+      <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
+      {/* category filtering */}
+      <div className="mb-8 flex items-center">
+        <select
+          name="category"
+          id="category"
+          className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
+        >
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default TopSellers;
