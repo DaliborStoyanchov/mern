0812ddb6a6bf7ba8d4dd2b2107fe213d8a76da4c
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-10-10 11:06:47 +0200

    feat: add books.json in public folder, fetch books in top selers

--- book-store-app/frontend/src/pages/Home/TopSellers.tsx
+++ book-store-app/frontend/src/pages/Home/TopSellers.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const categories = [
   "Choose a genre",
   "Business",
@@ -7,14 +9,21 @@ const categories = [
 ];
 
 const TopSellers = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="py-10">
       <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
-      {/* category filtering */}
       <div className="mb-8 flex items-center">
         <select
           name="category"
-          id="category"
+          id="category "
           className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
         >
           {categories.map((category, index) => (
