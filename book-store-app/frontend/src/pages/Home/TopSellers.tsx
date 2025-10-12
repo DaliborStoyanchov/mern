e2c8e9d3d1ae699e97c725196c5d36a9d8a3eea2
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-10-12 18:48:51 +0200

    feat: map selected books

--- book-store-app/frontend/src/pages/Home/TopSellers.tsx
+++ book-store-app/frontend/src/pages/Home/TopSellers.tsx
@@ -10,18 +10,29 @@ const categories = [
 
 const TopSellers = () => {
   const [books, setBooks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
 
   useEffect(() => {
     fetch("books.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setBooks(data));
   }, []);
+
+  const filteredBooks =
+    selectedCategory === "Choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
+
+  console.log(filteredBooks);
 
   return (
     <div className="py-10">
       <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
       <div className="mb-8 flex items-center">
         <select
+          onChange={(e) => setSelectedCategory(e.target.value)}
           name="category"
           id="category "
           className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
@@ -33,6 +44,9 @@ const TopSellers = () => {
           ))}
         </select>
       </div>
+      {filteredBooks.map((book, index) => (
+        <p key={index}>{book.title}</p>
+      ))}
     </div>
   );
 };
