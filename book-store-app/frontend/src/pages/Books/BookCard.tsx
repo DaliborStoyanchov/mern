d8f92facd12b1639e295d2e8d13d3f37975de271
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-10-16 00:11:18 +0200

    feat: add links to book card, add style, connect to books array

--- book-store-app/frontend/src/pages/Books/BookCard.tsx
+++ book-store-app/frontend/src/pages/Books/BookCard.tsx
@@ -1,36 +1,47 @@
 import { FiShoppingCart } from "react-icons/fi";
 import getImgUrl from "../../utils/getImg";
 import type { Book } from "../../models/Book";
+import { Link } from "react-router-dom";
 interface BookCardProps {
   book: Book;
 }
 
 const BookCard = ({ book }: BookCardProps) => {
   return (
-    <div className=" rounded-lg transition-shadow duration-300">
-      <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
+    <div className="rounded-lg transition-shadow duration-300">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:h-72 sm:justify-center gap-4">
         <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
-          <a href="/">
+          <Link to={`/books/${book._id}`}>
             <img
               src={`${getImgUrl(book.coverImage)}`}
               alt="book cover"
               className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
             />
-          </a>
+          </Link>
         </div>
-      </div>
-      <div>
-        <a href="/">
-          <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-            Book Title
-          </h3>
-        </a>
-        <p className="text-gray-600 mb-5">Book Description</p>
-        <p className="font-medium mb-5">$100</p>
-        <button className="bg-[#FFCE1A] px-12 py-2 rounded-md text-base font-[#0D0842] font-bold hover:bg-[#0D0842] hover:text-white transition-all duration-200 cursor-pointer space-x-1 flex items-center gap-1">
-          <FiShoppingCart />
-          <span>Add to Card</span>
-        </button>
+
+        <div>
+          <Link to={`/books/${book._id}`}>
+            <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
+              {book.title}
+            </h3>
+          </Link>
+          <p className="text-gray-600 mb-5">
+            {book.description.length > 80
+              ? `${book.description.slice(0, 80)}...`
+              : book.description}
+          </p>
+          <p className="font-medium mb-5">
+            ${book.newPrice}
+            <span className="line-through font-normal ml-2">
+              ${book.oldPrice}
+            </span>
+          </p>
+          <button className="bg-[#FFCE1A] px-12 py-2 rounded-md text-base font-[#0D0842] font-bold hover:bg-[#0D0842] hover:text-white transition-all duration-200 cursor-pointer space-x-1 flex items-center gap-1">
+            <FiShoppingCart />
+            <span>Add to Card</span>
+          </button>
+        </div>
       </div>
     </div>
   );
