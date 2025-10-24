7239b52bb3eec288a3d5839bb4f129843a821512
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-10-24 19:41:18 +0200

    feat: news section done

--- book-store-app/frontend/src/pages/Home/News.tsx
+++ book-store-app/frontend/src/pages/Home/News.tsx
@@ -1,7 +1,15 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import news1 from "../../assets/news/news-1.png";
 import news2 from "../../assets/news/news-2.png";
 import news3 from "../../assets/news/news-3.png";
 import news4 from "../../assets/news/news-4.png";
+import { Link } from "react-router-dom";
 
 const news = [
   {
@@ -43,8 +51,53 @@ const news = [
 
 const News = () => {
   return (
-    <div>
+    <div className="py-16">
       <h2 className="text-3xl font-semibold mb-6">News</h2>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          1072: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1600: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {news.map((item, i) => (
+          <SwiperSlide key={i}>
+            <div className="flex flex-col sm:flex-row sm: justify-between items-center gap-12">
+              <div className="py-4">
+                <Link to={"/"}>
+                  <h3 className="text-lg font-medium hover:text-blue-600 mb-4">
+                    {item.title}
+                  </h3>
+                </Link>
+                <div className="w-12 h-[4px] bg-[#FFCE1A] mb-5"></div>
+                <p className="text-sm text-gray-600">{item.description}</p>
+              </div>
+              <div className="flex-shrink-0">
+                <img
+                  src={item.image}
+                  alt="image of news"
+                  className="w-full object-cover"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
