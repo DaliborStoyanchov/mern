84505b1c8246319fa895976b90aba522875b2aff
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-11-18 06:03:29 +0100

    feat: add register page

--- book-store-app/frontend/src/components/Login/Login.tsx
+++ book-store-app/frontend/src/components/Login/Login.tsx
@@ -15,6 +15,10 @@ const Login = () => {
 
   const onSubmit = (data: any) => console.log(data);
 
+  const handleGoogleSignIn = () => {
+    console.log("GOOGLE SIGN IN");
+  };
+
   return (
     <div className="h-[calc(100vh-320px)] flex justify-center items-center">
       <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -71,7 +75,10 @@ const Login = () => {
           </Link>{" "}
         </p>
         <div className="mt-4">
-          <button className="w-full flex flex-wrap gap-1 items-center justify-center bg-[#0D0842] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none">
+          <button
+            onClick={handleGoogleSignIn}
+            className="w-full flex flex-wrap gap-1 items-center justify-center bg-[#0D0842] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          >
             <FaGoogle className="mr-2" /> Sign in with Google
           </button>
         </div>
