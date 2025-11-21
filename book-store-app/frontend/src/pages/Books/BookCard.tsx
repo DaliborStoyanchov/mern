c1cc33c5a7f2b32f97ba965829c45cbdcbd75120
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-11-21 12:16:43 +0100

    feat: add a dispatch to the book card, onlick event add to cart

--- book-store-app/frontend/src/pages/Books/BookCard.tsx
+++ book-store-app/frontend/src/pages/Books/BookCard.tsx
@@ -3,11 +3,19 @@ import { FiShoppingCart } from "react-icons/fi";
 import getImgUrl from "../../utils/getImg";
 import type { Book } from "../../models/Book";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 interface BookCardProps {
   book: Book;
 }
 
 const BookCard = ({ book }: BookCardProps) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (book: Book) => {
+    dispatch(addToCart(book));
+  };
+
   return (
     <div className="rounded-lg transition-shadow duration-300">
       <div className="flex flex-col sm:flex-row sm:items-center sm:h-72 sm:justify-center gap-4">
@@ -38,7 +46,10 @@ const BookCard = ({ book }: BookCardProps) => {
               ${book.oldPrice}
             </span>
           </p>
-          <button className="bg-[#FFCE1A] px-12 py-2 rounded-md text-base font-[#0D0842] font-bold hover:bg-[#0D0842] hover:text-white transition-all duration-200 cursor-pointer space-x-1 flex items-center gap-1">
+          <button
+            onClick={() => handleAddToCart(book)}
+            className="bg-[#FFCE1A] px-12 py-2 rounded-md text-base font-[#0D0842] font-bold hover:bg-[#0D0842] hover:text-white transition-all duration-200 cursor-pointer space-x-1 flex items-center gap-1"
+          >
             <FiShoppingCart />
             <span>Add to Card</span>
           </button>
