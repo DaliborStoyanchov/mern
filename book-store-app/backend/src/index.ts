edf6f3bf23d0d55e35c598ab09989abb93cef00b
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-12-07 22:30:49 +0100

    feat: add nodemon

--- book-store-app/backend/src/index.ts
+++ book-store-app/backend/src/index.ts
@@ -4,10 +4,12 @@ import type { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 
+const port = 3000;
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript + Express!");
 });
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log(`Server is up and running on port: ${port}`);
 });
