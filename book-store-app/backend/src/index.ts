7463d442320486e0754d3cba4ca04fb2a93254b3
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-12-12 05:08:58 +0100

    feat: add cors dependency

--- book-store-app/backend/src/index.ts
+++ book-store-app/backend/src/index.ts
@@ -2,9 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
+
 import { connectDB } from "./db.ts";
 
 const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 const port = process.env.PORT || 3000;
 
