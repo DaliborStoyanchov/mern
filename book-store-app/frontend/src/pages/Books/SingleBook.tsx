82710668e6598ac8b6031a34c562e71cfe606a60
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-12-27 22:10:45 +0100

    feat: add redux dispatch and handle add to cart for single book component

--- book-store-app/frontend/src/pages/Books/SingleBook.tsx
+++ book-store-app/frontend/src/pages/Books/SingleBook.tsx
@@ -2,10 +2,19 @@ import { useParams } from "react-router-dom";
 import { useFetchBookByIdQuery } from "../../redux/features/books/booksApi";
 import getImgUrl from "../../utils/getImg";
 import { FiShoppingCart } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
+import type { Book } from "../../models/Book";
 
 const SingleBook = () => {
   const { id } = useParams();
   const { data: book, isLoading, isError } = useFetchBookByIdQuery(id);
+
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (book: Book) => {
+    dispatch(addToCart(book));
+  };
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -43,8 +52,12 @@ const SingleBook = () => {
             {book?.description}
           </p>
         </div>
-        <button>
+        <button
+          onClick={() => handleAddToCart(book)}
+          className="bg-[#FFCE1A] px-12 py-2 rounded-md text-base font-[#0D0842] font-bold hover:bg-[#0D0842] hover:text-white transition-all duration-200 cursor-pointer space-x-1 flex items-center gap-1"
+        >
           <FiShoppingCart />
+          <span>Add to Cart</span>
         </button>
       </div>
     </div>
